284596462e3818b51f8be60ddf61d2034a30606a
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2017-08-07 13:03:38 +0530

    change socket logs from error to logs

--- controllers/socket/main.js
+++ controllers/socket/main.js
@@ -5,12 +5,12 @@ let socketConnections = {};
 module.exports = {
     addSocket : function(data, cb){
     	socketConnections[data.socket] = {};
-        console.error('[SOCKET][MAIN] addSocket', data.socket)
+        console.log('[SOCKET][MAIN] addSocket', data.socket)
         cb(_.size(socketConnections));
     },
     removeSocket : function(data, cb){
     	delete socketConnections[data.socket];
-        console.error('[SOCKET][MAIN] removeSocket', data.socket)
+        console.log('[SOCKET][MAIN] removeSocket', data.socket)
         cb(_.size(socketConnections));
     }
 }
