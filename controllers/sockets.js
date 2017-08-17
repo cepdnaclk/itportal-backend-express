34fdca4aba6e4fa7542bab42cd123a11da51251e
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2017-08-17 11:53:16 +0530

    Remove logs on socket connection changes

--- controllers/sockets.js
+++ controllers/sockets.js
@@ -4,16 +4,16 @@ module.exports = function(http){
     var io = require('socket.io')(http);
 
     io.on('connection', function(socket){
-        console.log('[SOCKET.IO] a user connected');
+        // console.log('[SOCKET.IO] a user connected');
         main.addSocket({'socket': socket.id}, function(res){
             io.emit('user_connected', {user_count: res});
-            console.log('[SOCKET.IO] user connected');
+            // console.log('[SOCKET.IO] user connected');
         })
 
         socket.on('disconnect', function(){
         	main.removeSocket({'socket': socket.id}, function(res){
 	            io.emit('user_disconnected', {user_count: res});
-	            console.log('[SOCKET.IO] user disconnected');
+	            // console.log('[SOCKET.IO] user disconnected');
 
         	})
         })
