d1af65a282e010ff8bc98cab425d073e95297c30
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2017-09-27 04:21:47 +0530

    Data model for interviewer feedback
    Temporary version, change as needed

---
+++ models/interviews/interviewer_feedback.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// define the schema for our Offer model
+var OfferSchema = mongoose.Schema({
+
+    student: { type: Schema.Types.ObjectId, ref: 'User' },
+    company: { type: Schema.Types.ObjectId, ref: 'Organization' },
+    accepted: { type: Object }
+
+}, {
+    timestamps: true
+});
+
+// create the model for Offers and expose it to our app
+module.exports = mongoose.model('Offer', OfferSchema);
